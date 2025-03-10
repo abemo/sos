3263eb3e43ffdf46452eb5eabb714c3aed656acc
TobiasJay <143559594+TobiasJay@users.noreply.github.com>
2025-03-10 10:37:56 -0700

    Default pages for food, wellness, housing. As well as donate and volunteer pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import Header from "@/components/Header";
+import { SiteHeader } from "@/components/site-header";
 import "./globals.css";
 
 import Footer from "@/components/Footer";
@@ -22,7 +22,18 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="flex flex-col min-h-screen w-full">
-            <main className="flex-1 w-full">{children}</main>
+          <div className="[--header-height:calc(theme(spacing.14))]">
+            <SidebarProvider className="flex flex-col">
+              <SiteHeader />
+              <div className="flex flex-1">
+                <AppSidebar />
+                <SidebarInset>
+                  {/* main content */}
+                  <main className="flex-1 w-full">{children}</main>
+                </SidebarInset>
+              </div>
+            </SidebarProvider>
+          </div>
 
             {/* Custom Footer */}
             <Footer />
